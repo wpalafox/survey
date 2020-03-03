e3e0a926aceb9177f4728e6926b886be05af0074
wpalafox <wpalafox@gmail.com>
2020-03-02 20:36:23 -0600

    building a survey

---
+++ src/components/Form.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import './form.css'
+
+export default class Form extends Component {
+    render() {
+        return (
+            <div className="container">
+                
+                <p id="name-input">Full Name</p>
+                <input type="text"/> 
+
+                <p id="email-input">Account Email</p>
+                <input type="text"/> 
+
+                <p id="email-input">Organization Name</p>
+                <input type="text"/> 
+
+                
+            
+            
+            
+            
+            
+            
+            
+            </div>
+        )
+    }
+}
