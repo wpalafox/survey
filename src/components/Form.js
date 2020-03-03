9391d0cbc1c9abde45791c37d0719971181ab747
wpalafox <wpalafox@gmail.com>
2020-03-03 06:17:08 -0600

    finish building the form html

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,26 +4,53 @@ import './form.css'
 export default class Form extends Component {
     render() {
         return (
+            
+        <div className="container">  
+       
             <div className="container">
+                    
+                    <div className="form-group">
+                        <p id="name-p">Full Name</p>
+                        <input id="name-input" type="text"/> 
+                    </div>
+                    
+                    <div className="form-group">   
+                        <p id="email-p">Account Email</p>
+                        <input id="email-input" type="text"/> 
+                    </div>
+                    
+                    <div className="form-group">   
+                        <p id="organization-p">Organization Name</p>
+                        <input id="organization-input" type="text"/> 
+                    </div>
+                    
+                    <div className="form-group">
+                        <label for="labelForTypeOfIssue">Type Of Issue</label>
+                        <select class="form-control" id="formcontrolselect">
+                            <option>Design Canvas</option>
+                            <option>Integrations</option>
+                            <option>Analytics</option>
+                            <option>Commerce AI</option>
+                            <option>Promotions Area</option>
+                        </select>
+                    </div>
+
+                    <div className="form-group"> 
+                        <p id="promotion-p">Promotion ID</p>
+                        <input id="promotion-input" type="text"/> 
+                    <div/>
                 
-                <p id="name-input">Full Name</p>
-                <input type="text"/> 
-
-                <p id="email-input">Account Email</p>
-                <input type="text"/> 
-
-                <p id="email-input">Organization Name</p>
-                <input type="text"/> 
-
+                    <div className="form-group"> 
+                        <p id="issue-p">Detailed Description of Issue</p>
+                        <textarea id="issue-input"/> 
+                    </div>
                 
-            
-            
-            
-            
-            
-            
-            
             </div>
+
+            <button type="button" class="btn btn-danger">Submit</button>
+         
+        </div>  
+     </div>
         )
     }
 }
